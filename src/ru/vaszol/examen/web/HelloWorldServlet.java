91349b0204d3f4059250ede22bc6122ed1489085
vaszol <vaszol@mail.ru>
2015-04-05 17:11:43 +0300

    работа с сервлетом, размещение на apache Tomcat

---
+++ src/ru/vaszol/examen/web/HelloWorldServlet.java
@@ -0,0 +1,22 @@
+package ru.vaszol.examen.web;
+
+/**
+ * Created by vas on 05.04.15.
+ */
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html;charset=utf-8");
+
+        PrintWriter pw = resp.getWriter();
+        pw.println("<H1>Hello, world! или Привет мир</H1>");
+
+    }
+}
